8066a0b2b1157c46ead10b8ed2749e40208c6970
ternence-wxy <weiqsctj@126.com>
2022-01-21 19:33:54 +0800

    添加用户列表

--- src/components/List.vue
+++ src/components/List.vue
@@ -10,19 +10,20 @@
 <template>
 	<div class="row">
 		<!-- 展示用户列表 -->
-
 		<div v-show="info.users.length" class="card" v-for="user in info.users" :key="user.login">
 			<a :href="user.html_url" target="_blank">
 				<img :src="user.avatar_url" style='width: 100px'/>
 			</a>
 			<p class="card-text">{{user.login}}</p>
 		</div>
+
 		<!-- 展示欢迎词 -->
 		<h1 v-show="info.isFirst">欢迎使用！</h1>
 		<!-- 展示加载中 -->
 		<h1 v-show="info.isLoading">加载中....</h1>
 		<!-- 展示错误信息 -->
 		<h1 v-show="info.errMsg">{{info.errMsg}}</h1>
+
 	</div>
 </template>
 
