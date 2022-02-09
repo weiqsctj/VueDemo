1f64ce9f9a9eaa32eeb08bbb46027d93780e8b4a
ternence-wxy <weiqsctj@126.com>
2022-02-09 14:33:48 +0800

    搭建vuex环境

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
+//引入插件
+import vueResource from 'vue-resource'
+//引入vuex
+import Vuex from 'vuex'
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-
+//使用插件
+Vue.use(vueResource)
+//使用vuex
+Vue.use(Vuex)
 //创建vm
 new Vue({
 	el:'#app',
